de477d5ff864874611376148000363ad06e9d586
Jorge Esteban Zaragoza Salazar <thesuperjez@mx-it04876.globant.com>
2024-02-08 15:29:23 -0600

    Maximum String

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,18 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function getMaximumFromMap(map) {
+    let maximum = 0;
+    Object.keys(map).forEach((char) => maximum = map[char] > maximum ? char : maximum);
+    return maximum;
+}
+
+function maxChar(str) {
+    const charMap = {};
+    str.split('').forEach((singleChar) => {
+        charMap[singleChar] = !charMap[singleChar] ? 1 : ++charMap[singleChar];
+    });
+    return getMaximumFromMap(charMap);
+}
 
 module.exports = maxChar;
